b4ab799feed38b62456a158178ee12ad9e9541ff
ANSH BHARDWAJ <tigon00400040@gmail.com>
2024-12-20 18:33:43 +0530

    Changed the ComileSDK version from 34 to 35

--- app/src/main/java/com/example/to_do/MainActivity.kt
+++ app/src/main/java/com/example/to_do/MainActivity.kt
@@ -4,11 +4,14 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.activity.enableEdgeToEdge
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -27,9 +30,20 @@ class MainActivity : ComponentActivity() {
     }
 }
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun MainPage() {
+   Scaffold(
+       topBar = {
+           TopAppBar(title = { Text(text = "ToDo")}
 
+           )
+       }, content = { innerPadding ->
+           Column(modifier = Modifier.padding(innerPadding)) {
+
+           }
+       }
+   )
 }
 
 @Preview(showBackground = true)
